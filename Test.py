09ba3019efe9d304a13279cf376d8ca64a33427b
rafikt1992 <rafik.z1992@gmail.com>
2019-06-01 18:35:06 +0200

    ADDED test for arguments

---
+++ Test.py
@@ -0,0 +1,14 @@
+import sys
+import argparse
+
+ap = argparse.ArgumentParser(description='Convert Hex Files')
+ap.add_argument("--name", help="name of the user", type=str, required=True)
+ap.add_argument("--second", type=str, required= True)
+
+#args =  vars(ap.parse_args())
+args = ap.parse_args()
+print(args.name)
+
+print("bla bla {} second {}".format(args.name, args.second))
+
+
